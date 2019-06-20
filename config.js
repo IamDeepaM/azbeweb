bcd589da1884d8a11f71c4c7e04e1760d0aa3325
yogeshds10 <yogeshds10@gmail.com>
2019-06-20 13:11:25 +0530

    config setup

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+var baseUrl = 'http://localhost:3000'; // Local
+// var baseUrl = 'http://localhost:3000'; // Production
+
 module.exports = {
   database: 'mongodb://127.0.0.1:27017/amz',
   isProd: false,
+  baseUrl: baseUrl
 }
