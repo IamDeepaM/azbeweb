5ca8dd73ae2ed437158b24cf08b6f5fc1ce9dc49
yogeshds10 <yogeshds10@gmail.com>
2019-07-16 23:55:49 +0530

    uat port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var baseUrl = 'http://localhost:3000'; // Local
-// var baseUrl = 'http://localhost:3000'; // Production
+// var baseUrl = 'http://176.223.135.127:9000'; // Production
 
 module.exports = {
   database: 'mongodb://127.0.0.1:27017/amz',
